45444481d3be751ead94b595b8f5dc8ee46861aa
Vitaliy Potapov <granddad.po@gmail.com>
2024-03-04 13:22:08 +0400

    feat: animated opacity for comics list

--- src/components/comicsList/ComicsList.jsx
+++ src/components/comicsList/ComicsList.jsx
@@ -5,63 +5,69 @@ import './comicsList.scss';
 import Spinner from "../spinner/Spinner.jsx";
 import ErrorMessage from "../error/ErrorMessage.jsx";
 import { Link } from "react-router-dom";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 
 const ComicsList = () => {
 
 	const [comics, setComics] = useState([])
-    const [offset, setOffset] = useState(1200)
-    const [loadingNewComics, setLoadingNewComics] = useState(false)
+	const [offset, setOffset] = useState(1200)
+	const [loadingNewComics, setLoadingNewComics] = useState(false)
 	const {loading, error, getAllComics} = useMarvelService()
 
 	useEffect(() => {
 		return () => {
-            onRequestNewComics(offset, true)
+			onRequestNewComics(offset, true)
 		}
 	}, []);
 
 	const onComicListLoaded = (newComicsArray) => {
 		setComics(comics => [...comics, ...newComicsArray])
-        setOffset(offset => offset + 8)
-        setLoadingNewComics(false)
+		setOffset(offset => offset + 8)
+		setLoadingNewComics(false)
 	}
 
-    const onRequestNewComics = (offset, initial) => {
-        initial ? setLoadingNewComics(false) : setLoadingNewComics(true)
-        getAllComics(offset)
-            .then(onComicListLoaded)
-    }
+	const onRequestNewComics = (offset, initial) => {
+		initial ? setLoadingNewComics(false) : setLoadingNewComics(true)
+		getAllComics(offset)
+			.then(onComicListLoaded)
+	}
 
 	const renderItems = (comics) => {
 		const items = comics.map(({id, title, thumbnail, price}) => {
 			return (
-				<li key={id} className="comics__item">
-					<Link to={`/comics/${id}`}>
-						<img src={thumbnail} alt={title} className="comics__item-img"/>
-						<div className="comics__item-name">{title}</div>
-						<div className="comics__item-price">{price}$</div>
-					</Link>
-				</li>
+				<CSSTransition key={id} timeout={500} classNames="comics__item">
+					<li className="comics__item">
+						<Link to={`/comics/${id}`}>
+							<img src={thumbnail} alt={title} className="comics__item-img"/>
+							<div className="comics__item-name">{title}</div>
+							<div className="comics__item-price">{price}$</div>
+						</Link>
+					</li>
+				</CSSTransition>
 			)
 		})
 
 		return (
 			<ul className="comics__grid">
-				{items}
+				<TransitionGroup component={null}>
+					{items}
+				</TransitionGroup>
 			</ul>
 		)
 	}
 
 	const items = renderItems(comics)
 
-    const spinner = (loading && !loadingNewComics) ? <Spinner/> : null
-    const errorMessage = error ? <ErrorMessage/> : null
+	const spinner = (loading && !loadingNewComics) ? <Spinner/> : null
+	const errorMessage = error ? <ErrorMessage/> : null
 
 	return (
 		<div className="comics__list">
-            {spinner}
-            {errorMessage}
+			{spinner}
+			{errorMessage}
 			{items}
-			<button onClick={() => onRequestNewComics(offset)} className="button button__main button__long" disabled={loadingNewComics}>
+			<button onClick={() => onRequestNewComics(offset)} className="button button__main button__long"
+					disabled={loadingNewComics}>
 				<div className="inner">load more</div>
 			</button>
 		</div>
